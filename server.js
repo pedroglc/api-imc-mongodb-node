0c2f38d5cb76e613c81092c50b83445ada3f1f06
Pedro <pedrogustavo.1996@hotmail.com>
2024-03-31 17:16:14 -0300

    alterado variavel porta no server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 connectDatabase()
   .then(() => {
-    app.listen(5000, () =>
+    app.listen(port, () =>
       console.log('Servidor rodando na porta 5000 e banco de dados conectado')
     );
   })
